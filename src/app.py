f805ec728b7fff80bf6a2be093bad1c4549c8753
Matthias Deiml <matthias@deiml.net>
2020-01-10 22:31:52 +0100

    Simple server

--- src/app.py
+++ src/app.py
@@ -1,1 +1,21 @@
-print("test")
+import socketserver
+
+class BlubbelHandler(socketserver.BaseRequestHandler):
+
+    def handle(self):
+        print('test')
+        # Receive 1024 bytes of data
+        data = self.request.recv(1024)
+
+        # For now just print the data and echo it
+        print(data)
+        self.request.sendall(data)
+
+if __name__ == '__main__':
+    # Create server at port 8000
+    server = socketserver.TCPServer(('0.0.0.0', 8000), BlubbelHandler)
+
+    print('Starting server...')
+
+    # Run server until application is terminated
+    server.serve_forever()
